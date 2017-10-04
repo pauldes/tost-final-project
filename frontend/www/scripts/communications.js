2af601a2bcecd635bf0791e058004d773de02c00
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-04 02:36:40 +0200

    com

---
+++ frontend/www/scripts/communications.js
@@ -0,0 +1,5 @@
+﻿function checkConnection() {
+    // Besoin de cordova-plugin-network-information pour ça
+    var networkState = navigator.connection.type;
+    return ('Connection type: ' + navigator.connection.type);
+}
