99c25349fd927ebb4fa93aee53d3b594e69a20a8
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-27 10:35:41 +0200

    com frontend-RestAPI

--- backend/src/main/java/com/tost/serv/HomeServlet.java
+++ backend/src/main/java/com/tost/serv/HomeServlet.java
@@ -4,13 +4,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.io.IOException;
 
-@Path("")
+
+@Path("/home")
 public class HomeServlet extends HttpServlet {
+    @GET
+    public String yesyes(){
+        return "I'm Paul. This is my API rest.";
+    }
+
+    @GET
+    @Path("/nono")
+    public String nono(){
+        return "I'm Paul. This is my API rest.";
+    }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("=)");
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Momomo");
     }
 }
--- frontend/www/scripts/communications.js
+++ frontend/www/scripts/communications.js
@@ -3,3 +3,21 @@
     var networkState = navigator.connection.type;
     return ('Connection type: ' + navigator.connection.type);
 }
+
+function initAxios() {
+    var instance = axios.create({
+        baseURL: 'http://localhost:8080/api',
+        timeout: 1000,
+        headers: {}
+    })
+}
+
+function getHome(){
+    axios.get('http://localhost:8080/api/home') //TODO: it should be only /home
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
